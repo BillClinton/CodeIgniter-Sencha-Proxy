f6a048ed516d8295868a3c33d595a51562fbc299
BillClinton <bclinton@appunto.net>
2014-11-20 18:53:55 -0500

    adding source for example application

---
+++ CodeIgniter/application/views/example.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>CodeIgniter-Sencha-Proxy demo</title>
+
+	<?= senchaproxy_config() ?>
+
+	<link rel="stylesheet" type="text/css" href="<? echo base_url() ?>resources/example/css/icons.css" />
+	<link rel="stylesheet" type="text/css" href="<? echo base_url() ?>resources/example/theme/ProxyExample-all.css" />
+
+	<script type="text/javascript">
+		Ext = window.Ext || {}; Ext.Boot={};
+	</script> 
+	<script type="text/javascript" src="<? echo base_url() ?>resources/example/js/app.js"></script> 
+
+</head>
+<body>
+
+</body>
+</html>
